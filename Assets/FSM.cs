a61740740bd4ee4aa484c38a0609f5524d9cb65d
zhongxingyun <345786117@qq.com>
2016-10-02 06:18:03 +0800

    FSM
    最简易版FSM

---
+++ Assets/FSM.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 简易版 FSM
+/// </summary>
+public class FSM
+{
+    //定义状态切换回调函数
+    public delegate void FSMTranslationCallfunc();
+
+    /// <summary>
+    /// 状态类
+    /// </summary>
+    public class FSMState
+    {
+        public string name;
+
+        public FSMState(string name)
+        {
+            this.name = name;
+        }
+
+        //存储该状态的所有 状态切换条件
+        public Dictionary<string, FSMTranslation> TranslationDict = new Dictionary<string, FSMTranslation>();
+    };
+
+    /// <summary>
+    /// 状态切换条件类
+    /// </summary>
+    public class FSMTranslation
+    {
+        public string name;
+        public FSMState fromState;
+        public FSMState toState;
+        public FSMTranslationCallfunc callfunc;  //回调函数
+
+        public FSMTranslation(FSMState fromState,string name,FSMState toState,FSMTranslationCallfunc callfunc)
+        {
+            this.fromState = fromState;
+            this.name = name;
+            this.toState = toState;
+            this.callfunc = callfunc;
+        }
+    };
+
+    private static FSM instance;
+    private static object _lock = new object();
+    public static FSM Instance
+    {
+        get
+        { 
+            if(instance == null)
+            {
+                lock(_lock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new FSM();
+                    }
+                }
+            }
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+
+    private FSMState mCurrentState;
+    Dictionary<string, FSMState> stateDict = new Dictionary<string, FSMState>();
+
+    public void AddState(FSMState state)
+    {
+        stateDict[state.name] = state;
+    }
+
+    public void AddTranslation(FSMTranslation translation)
+    {
+        stateDict[translation.fromState.name].TranslationDict[translation.name] = translation;
+    }
+
+    //1,启动FSM状态机  2,调用FSM.AddState(),FSM.AddTranslation()添加状态和切换条件  3,调用FSM.HandleEvent()执行状态切换和行为操作
+    public void Init(FSMState state)
+    {
+        mCurrentState = state;
+    }
+
+    //执行状态跳转，并执行行为操作
+    public void HandleEvent(string name)
+    {
+        if(mCurrentState != null && mCurrentState.TranslationDict.ContainsKey(name))
+        {
+            Debug.LogWarning("fromState : " + mCurrentState.name);
+        }
+
+        mCurrentState.TranslationDict[name].callfunc();  //行为操作
+        mCurrentState = mCurrentState.TranslationDict[name].toState; //状态跳转
+
+        Debug.LogWarning("toState : " + mCurrentState.name);
+    }
+
+    ~FSM()
+    {
+        Instance = null;
+    }
+};
---
+++ Assets/TestFSM.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFSM : MonoBehaviour
+{
+	void Start () 
+    {
+        CreateFSMMachine();
+	}
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            FSM.Instance.HandleEvent("touch_down"); //runState -> jumpState
+        }
+        else if(Input.GetKeyDown(KeyCode.S))  
+        {
+            FSM.Instance.HandleEvent("touch_down"); //jumpState -> doubleJumpState
+        }
+        else if (Input.GetKeyDown(KeyCode.D))     
+        {
+            FSM.Instance.HandleEvent("land");  //jumpState/doubleJumpState -> runState
+        }
+    }
+
+
+    private void CreateFSMMachine()
+    {
+        //创建状态 
+        FSM.FSMState idleState = new FSM.FSMState("idle");
+        FSM.FSMState runState = new FSM.FSMState("run");
+        FSM.FSMState jumpState = new FSM.FSMState("jump");
+        FSM.FSMState doubleJumpState = new FSM.FSMState("double_jump");
+        FSM.FSMState dieState = new FSM.FSMState("die");
+
+        //创建跳转
+        FSM.FSMTranslation touchTranslation1 = new FSM.FSMTranslation(runState, "touch_down", jumpState, Jump);
+        FSM.FSMTranslation touchTranslation2 = new FSM.FSMTranslation(jumpState, "touch_down", doubleJumpState, DoubleJump);
+
+        FSM.FSMTranslation landTranslation1 = new FSM.FSMTranslation(jumpState, "land", runState, Run);
+        FSM.FSMTranslation landTranslation2 = new FSM.FSMTranslation(doubleJumpState, "land", runState, Run);
+
+        //为状态机添加状态
+        FSM.Instance.AddState(idleState);
+        FSM.Instance.AddState(runState);
+        FSM.Instance.AddState(jumpState);
+        FSM.Instance.AddState(doubleJumpState);
+        FSM.Instance.AddState(dieState);
+
+        //添加切换
+        FSM.Instance.AddTranslation(touchTranslation1);
+        FSM.Instance.AddTranslation(touchTranslation2);
+        FSM.Instance.AddTranslation(landTranslation1);
+        FSM.Instance.AddTranslation(landTranslation2);
+
+        //启动FSM状态机
+        FSM.Instance.Init(runState);
+    }
+
+    private void Jump()
+    {
+        Debug.Log("切换到跳跃状态");
+    }
+
+    private void DoubleJump()
+    {
+        Debug.Log("切换到双重跳跃状态");
+    }
+	
+    private void Run()
+    {
+        Debug.Log("切换到奔跑状态");
+    }
+	
+};
